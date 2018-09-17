c87d9a4df846e8c9c35bc8959c43c40ebf7f7d01
Luiz <Luiz@150.162.184.126>
2018-09-17 20:10:31 -0300

    commit local

--- src/entidades/Calculadora.java
+++ src/entidades/Calculadora.java
@@ -4,7 +4,8 @@ public class Calculadora {
 
 	private double numero;
 	private double soma;
-	
+	private double subtrair;
+	private double subtracao;
 	
 	public double getNumero() {
 		return numero;
