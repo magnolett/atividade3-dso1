9ba02e9537134678e686cdc1183902126ae70dc2
09390712904 <09390712904@ine>
2018-09-17 20:54:33 -0300

    Atualizando Helper novamente

--- src/entidades/Helper.java
+++ src/entidades/Helper.java
@@ -3,17 +3,7 @@ package entidades;
 public class Helper {
 
 	public static int doubleParaInt(Double numero) {
-
 		return numero.intValue();
-	}
-	
-	private static boolean isInteiro(Double numero) {
-		
-		if(numero.toString().split(".")[1].substring(0, 1).equals("0")) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 	
 	public static double intParaDouble(Integer numero) {
